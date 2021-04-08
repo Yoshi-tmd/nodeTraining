dab3649512fb4f1c69fe7715eb28b969aee811ab
tomoda <tomoda@na-s.jp>
2021-04-08 12:58:46 +0900

    Chap2-3_p88まで

--- chap2/app03.js
+++ chap2/app03.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
+
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
 
 let server = http.createServer(getFromClient);
 
@@ -10,10 +13,8 @@ console.log('Server running on port 3000.');
 
  // createServerの処理
 function getFromClient(req, res) {
-    fs.readFile('./index.html', 'UTF-8', 
-        (error, data) => {
-            res.writeHead(200, { 'Content-type': 'text/html' });
-            res.write(data);
-            res.end();
-    });
+    let content = ejs.render(index_page);
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.write(content);
+    res.end();
 }
